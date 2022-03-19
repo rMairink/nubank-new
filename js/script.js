a521f1ab353c81defa98a3cbc414ae096f620627
rMairink <dev.mairink@gmail.com>
2022-03-19 05:51:38 -0300

    alterações scroll

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-function valid() {
+function validButton() {
     let btnCPF = document.querySelector('#verificationBtn')
     let inputCPF = document.querySelector('#inputCPF').value
-    
-    if(inputCPF){
-        btnCPF.style = "background-color: rgb(130 6 208);"
-    }else{
-        btnCPF.style = 'background-color: #111111a'
-    }
 
+    if (inputCPF) {
+        btnCPF.style = "background-color: rgb(130 6 208);"
+    } else {
+        btnCPF.style = "background-color: #111111a;"
+    }
 }
 
 setInterval(() => {
-    valid()
+    validButton()
 }, 1000);
+
+// scroll position 700
+
+window.addEventListener('scroll', (event) => {
+    let headerMainScroll = document.querySelector('#headerMainScroll')
+    let imgRoxo = document.querySelector('#img-header-roxo')
+    let imgBranco = document.querySelector('#img-header-branco')
+    let queroSerNubankBtn = document.querySelector('.quero-ser-nubank')
+    let scroll = this.scrollY
+
+    if(scroll >= 700){
+        headerMainScroll.style = "position: fixed; background-color: #fff; margin: 0;"
+        imgRoxo.style = "display: flex;"
+        imgBranco.style = "display: none;"
+        queroSerNubankBtn.style = "display: flex;"
+
+    }else if(scroll < 700){
+        headerMainScroll.style = "position: initial;"
+        imgRoxo.style = "display: none;"
+        imgBranco.style = "display: flex;"
+        queroSerNubankBtn.style = "display: none;"
+    }else{
+        return
+    }
+})
